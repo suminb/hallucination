8e3865c14a5e1192be9c5bd04d8c515e8e581271
Sumin Byeon <suminb@gmail.com>
2019-11-24 19:05:22 +0900

    Reformat code

--- frontend.py
+++ frontend.py
@@ -1,22 +1,22 @@
-from hallucination import ProxyFactory
-from hallucination.models import Proxy
-from multiprocessing import Pool
-from threading import Thread, Lock
-from queue import Queue
-
 import getopt
-import os, sys
-import logging
 import json
+from queue import Queue
+import sys
+from threading import Thread
 
-logger = logging.getLogger('hallucination')
+import logging
+
+from hallucination import ProxyFactory
+
+
+logger = logging.getLogger("hallucination")
 logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(logging.INFO)
 
 config = {}
 
 # FIXME: This is not a good design
-url = 'http://translate.google.com'
+url = "http://translate.google.com"
 proxy_factory = None
 
 threads = 8
@@ -28,18 +28,17 @@ class TestrunThread(Thread):
 
         self.queue = queue
 
-        self.proxy_factory = ProxyFactory(config=dict(
-            db_uri=config['db_uri']),
-            logger=logger
+        self.proxy_factory = ProxyFactory(
+            config=dict(db_uri=config["db_uri"]), logger=logger
         )
 
     def run(self):
         while True:
-            logger.info('Qsize = {} (approx.)'.format(self.queue.qsize()))
+            logger.info("Qsize = {} (approx.)".format(self.queue.qsize()))
 
             url, proxy = self.queue.get()
 
-            logger.info('Test run: Fetching %s via %s' % (url, proxy))
+            logger.info("Test run: Fetching %s via %s" % (url, proxy))
             self.proxy_factory.make_request(url, proxy=proxy, timeout=10)
 
             self.queue.task_done()
@@ -51,12 +50,12 @@ def create():
 
 def _import(file_path):
     """Imports a list of proxy servers from a text file."""
-    proxy_factory.import_proxies(open(file_path, 'r'))
+    proxy_factory.import_proxies(open(file_path, "r"))
 
 
 def export(file_path):
     """Exports the list of proxy servers to the standard output."""
-    proxy_factory.export_proxies(open(file_path, 'w'))
+    proxy_factory.export_proxies(open(file_path, "w"))
 
 
 def select():
@@ -65,7 +64,8 @@ def select():
 
 def evaluate():
     """
-    Selects proxy servers that have not been recently evaluated, and evaluates each of them.
+    Selects proxy servers that have not been recently evaluated, and evaluates
+    each of them.
 
     Refered http://docs.python.org/2/library/queue.html for skeleton code.
     """
@@ -92,42 +92,42 @@ def parse_config(file_name):
 
 
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'cti:x:sd:E', ['config='])
+    opts, args = getopt.getopt(sys.argv[1:], "cti:x:sd:E", ["config="])
 
     rf = None
     params = []
     for o, a in opts:
-        if o == '-c':
+        if o == "-c":
             rf = create
-        elif o == '-t':
+        elif o == "-t":
             rf = testrun
-        elif o == '-i':
+        elif o == "-i":
             rf = _import
             params = [a]
-        elif o == '-x':
+        elif o == "-x":
             rf = export
             params = [a]
-        elif o == '-s':
+        elif o == "-s":
             rf = select
-        elif o == '-E':
+        elif o == "-E":
             rf = evaluate
 
-        elif o == '--config':
+        elif o == "--config":
             parse_config(a)
 
-        elif o == '-d':
-            config['db_uri'] = 'sqlite:///%s' % a
+        elif o == "-d":
+            config["db_uri"] = "sqlite:///%s" % a
 
     global proxy_factory
-    proxy_factory = ProxyFactory(config=dict(
-        db_uri=config['db_uri']),
-        logger=logger
+    proxy_factory = ProxyFactory(
+        config=dict(db_uri=config["db_uri"]), logger=logger
     )
 
-    if rf != None:
+    if rf is not None:
         rf(*params)
     else:
-        raise Exception('Runtime mode is not specified.')
+        raise Exception("Runtime mode is not specified.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
